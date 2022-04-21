ed5f9b48eb37f18375f40e59e26725c803482c98
Savinov <vy.savinov@gmail.com>
2022-04-21 19:35:11 +0800

    settings kafka and zookeeper

--- src/main/java/ru/savinov/buyhandler/BuyHandlerApplication.java
+++ src/main/java/ru/savinov/buyhandler/BuyHandlerApplication.java
@@ -9,5 +9,4 @@ public class BuyHandlerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BuyHandlerApplication.class, args);
 	}
-
 }
---
+++ src/main/java/ru/savinov/buyhandler/KafKaConsumerService.java
@@ -0,0 +1,15 @@
+package ru.savinov.buyhandler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class KafKaConsumerService {
+
+    @KafkaListener(topics = "cart", groupId = "group_id")
+    public void consume(String message) {
+        System.out.println(message);
+    }
+}
